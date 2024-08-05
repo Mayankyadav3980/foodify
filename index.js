ff1ee4b8b78df5c155626dff6aada2a412b2bb12
Mayankyadav3980 <ymayank501@gmail.com>
2024-08-05 22:09:18 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,32 +1,64 @@
-if (localStorage.getItem("wishlist") == null) {
-  localStorage.setItem("wislist", JSON.stringify([]));
-}
+// if (localStorage.getItem("wishlist") == null) {
+//   localStorage.setItem("wislist", JSON.stringify([]));
+// }
 
 const inputBox = document.getElementById("search-bar");
 const searchIcon = document.getElementById("search-icon");
 const resultsWrapper = document.getElementById("results");
 const cards_container = document.getElementById("cards_container");
-
-let searchText = "";
-
+const wishlistBtn = document.getElementById("wishlist-btn");
 //showMealList()
 inputBox.addEventListener("keyup", handleInput);
 searchIcon.addEventListener("click", handleSearch);
+wishlistBtn.addEventListener("click", handleWishlist);
 
-function hdp(obj) {
-  console.log("handleDP called " + obj);
+let searchText = "";
+
+function handleSearch(e) {
+  // getData(inputBox.value);
+  getMealDetails(1);
+  // console.log('searchIcon clicked');
+}
+
+async function getData(mealName) {
+  try {
+    let resObj = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/search.php?s=" + mealName
+    );
+    let { meals } = await resObj.json();
+    return meals;
+  } catch (e) {
+    console.log("err: " + e);
+  }
+}
+
+async function getMealDetails(id){
+  let obj = {};
+  try{
+    let url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772";
+    let resObj2 = await fetch(url);
+    let jsObj2 = await resObj2.json();
+    obj =  (jsObj2.meals[0]);
+  }catch(e){
+    console.log('error: ' + e);
+  }
+  return obj;
+  }
+
+async function test(id) {
+  console.log("handleDP called " + id );
   // let container = document.getElementById('meal-detail');
-  let container = document.getElementById("cards-container");
-  let details = ` 
-      <img id="meal-detail-img" src="${obj.strMealThumb}" alt="meal-image">
-        <div id="meal-txt">
-            <h1>${obj.strMeal}</h1>
-            <p>${obj.strInstructions}</p>
-            <a href="${obj.strYoutube}">watch video </a>
-        </div>
-  `;
+  // let container = document.getElementById("cards-container");
+  // let details = ` 
+  //     <img id="meal-detail-img" src="${obj.strMealThumb}" alt="meal-image">
+  //       <div id="meal-txt">
+  //           <h1>${obj.strMeal}</h1>
+  //           <p>${obj.strInstructions}</p>
+  //           <a href="${obj.strYoutube}">watch video </a>
+  //       </div>
+  // `;
 
-  container.innerHTML = details;
+  // container.innerHTML = details;
 }
 
 
@@ -51,7 +83,7 @@ async function handleInput(e) {
                 </div>
                 <p class="meal_name">${obj.strMeal}</p>
                 <div id='content'>
-                <button id="read-more-btn" onclick="hdp()"><a href="meal_details.html">Read More</a></button>
+                <button type="button" id="read-more-btn" onclick="test(${obj.idMeal})">rm</button>
                 <i class="fa-solid fa-heart " id="fav_btn"></i>
                 </div>
         </div>
@@ -64,7 +96,7 @@ async function handleInput(e) {
                 </div>
                 <p class="meal_name">${obj.strMeal}</p>
                 <div id='content'>
-                <button id="read-more-btn" onclick="handleDetailsPage(${obj.strMeal})">ṚM</button>
+                <button type="button" id="read-more-btn" onclick="test(${obj.idMeal})">rm</button>
                 <i class="fa-regular fa-heart " id="fav_btn"></i>
                 </div>
         </div>
@@ -76,25 +108,28 @@ async function handleInput(e) {
   cards_container.innerHTML = cards;
 }
 
-function handleSearch(e) {
-  // console.log(searchText);
-  // searchText = inputBox.value;
-  getData(inputBox.value);
-}
 
-async function getData(mealName) {
-  try{
-    let resObj = await fetch(
-    "https://www.themealdb.com/api/json/v1/1/search.php?s=" + mealName
-    );
-    let { meals } = await resObj.json();
-    // console.log(meals);
-    return meals;
-  }catch(e){
-    console.log('err: ' + e);
-  }
+function handleWishlist(){
+  let cards = '';
+  let obj = getMealDetails(1);
+
+
+
+  cards_container.innerHTML = `
+    <div class="card">
+                <div >
+                    <img class="meal_image" src="${obj.strMealThumb}" alt="meal-image">
+                </div>
+                <p class="meal_name">${obj.strMeal}</p>
+                <div id='content'>
+                <button type="button" id="read-more-btn" onclick="test(${obj.idMeal})">rm</button>
+                <i class="fa-solid fa-heart " id="fav_btn"></i>
+                </div>
+        </div>
+  `;
   
 }
+
 
 
 
